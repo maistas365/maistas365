8dc1281b15cfb4c344b54633d69b345f9942c20a
elevinas123 <elvinas.monstvilas@gmail.com>
2024-07-08 22:26:10 +0300

    finalized adding shadcn

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
     return (
         <div className="bg-red-500">
             <div>WTF?</div>
+            <Button>Labas</Button>
         </div>
     );
 }
