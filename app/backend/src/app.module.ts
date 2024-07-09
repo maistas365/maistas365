e02498b67dfb68137e280cc4c7c190a8b333ae30
81GB3N <jonas.balukonis@gmail.com>
2024-07-09 11:20:08 +0300

    configured app.module

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, 
+    ProductsModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true })]
 })
 export class AppModule {}
