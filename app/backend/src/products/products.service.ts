2cf1f9fed702b80a2a6006d3f0534ce1855fb59f
81GB3N <jonas.balukonis@gmail.com>
2024-07-17 14:55:47 +0300

    reformated file

--- app/backend/src/products/products.service.ts
+++ app/backend/src/products/products.service.ts
@@ -4,23 +4,30 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
-    constructor(private prisma: PrismaService, private config: ConfigService) {}
+  constructor(
+    private prisma: PrismaService,
+    private config: ConfigService,
+  ) {}
 
-    //get all of the products from the database
-    async getAllProducts(){
-        const allProducts = await this.prisma.product.findMany()
-        return allProducts;
-    }
+  //get all of the products from the database
+  async getAllProducts() {
+    const allProducts = await this.prisma.product.findMany({
+      include: {
+        priceHistory: true,
+        price: true,
+      },
+      take: 100,
+    });
+    return allProducts;
+  }
 
-    async getAllPrices(){
-        const allPrices = await this.prisma.product.findMany({
-            select:{
-                price: true,
-                name: true
-            }
-        })
-        return allPrices;
-    }
-
-
+  async getAllPrices() {
+    const allPrices = await this.prisma.product.findMany({
+      select: {
+        price: true,
+        name: true,
+      },
+    });
+    return allPrices;
+  }
 }
